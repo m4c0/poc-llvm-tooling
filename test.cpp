080591e5c7aa38e25494ea50a895e23c6d7b3188
Eduardo Costa <m4c0@users.noreply.github.com>
2023-08-17 12:28:20 +0100

    test: using "tooling" to emit object file (almost there)

--- test.cpp
+++ test.cpp
@@ -1,6 +1,11 @@
+#include "clang/CodeGen/CodeGenAction.h"
 #include "clang/Driver/Compilation.h"
 #include "clang/Driver/Driver.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/CompilerInvocation.h"
+#include "clang/Frontend/FrontendAction.h"
 #include "clang/Frontend/TextDiagnosticPrinter.h"
+#include "clang/Lex/LexDiagnostic.h"
 #include "clang/Lex/Preprocessor.h"
 #include "llvm/Support/VirtualFileSystem.h"
 #include <iostream>
@@ -8,8 +13,30 @@
 using namespace clang::driver;
 using namespace clang;
 
+class ExamplePragmaHandler : public PragmaHandler {
+public:
+  ExamplePragmaHandler() : PragmaHandler("uga") {}
+  void HandlePragma(Preprocessor &PP, PragmaIntroducer Introducer,
+                    Token &PragmaTok) {
+    Token Tok;
+    PP.LexUnexpandedToken(Tok);
+    if (!Tok.isAnyIdentifier())
+      PP.Diag(Tok, diag::err_pp_identifier_arg_not_identifier) << Tok.getKind();
+  }
+};
+
+class MyAction : public EmitObjAction {
+public:
+  bool BeginSourceFileAction(CompilerInstance &CI) override {
+    CI.getPreprocessor().AddPragmaHandler("example",
+                                          new ExamplePragmaHandler());
+
+    return EmitObjAction::BeginSourceFileAction(CI);
+  }
+};
+
 int main() {
-  llvm::StringRef clang_exe = "/usr/local/opt/llvm/bin/clang++";
+  constexpr const auto clang_exe = "/usr/local/opt/llvm/bin/clang++";
   std::string triple = "wasm32-wasi";
   std::string title = "poc clang driver";
 
@@ -19,11 +46,10 @@ int main() {
   auto diag_cli = new TextDiagnosticPrinter(llvm::errs(), &*diag_opts);
 
   DiagnosticsEngine diags{diag_ids, diag_opts, diag_cli};
-
   Driver driver{clang_exe, triple, diags, title};
 
   std::vector<const char *> args{};
-  args.push_back("clang++");
+  args.push_back(clang_exe);
   args.push_back("-std=c++20");
   args.push_back("-c");
   args.push_back("hello.cpp");
@@ -36,9 +62,33 @@ int main() {
     // do its job informing the user
     return 1;
 
-  llvm::SmallVector<std::pair<int, const Command *>, 4> fail_cmds;
-  auto result = driver.ExecuteCompilation(*c, fail_cmds);
+  auto cc1_args = c->getJobs().getJobs()[0]->getArguments();
+
+  auto files = IntrusiveRefCntPtr<FileManager>(new FileManager({}));
+  auto cdiag =
+      CompilerInstance::createDiagnostics(&*diag_opts, diag_cli, false);
+
+  SourceManager src_mgr(*cdiag, *files);
+  cdiag->setSourceManager(&src_mgr);
+
+  auto cinv = std::make_shared<CompilerInvocation>();
+  CompilerInvocation::CreateFromArgs(*cinv, cc1_args, *cdiag);
+
+  auto pch_opts = std::make_shared<clang::PCHContainerOperations>();
+  CompilerInstance cinst{pch_opts};
+  cinst.setInvocation(cinv);
+  cinst.setFileManager(&*files);
+  cinst.createDiagnostics(diag_cli, false);
+  cinst.createSourceManager(*files);
+
+  MyAction a{};
+  cinst.ExecuteAction(a);
+
+  files->clearStatCache();
+
+  // llvm::SmallVector<std::pair<int, const Command *>, 4> fail_cmds;
+  // auto result = driver.ExecuteCompilation(*c, fail_cmds);
 
   llvm::llvm_shutdown();
-  return result == 0 ? 0 : 1;
+  // return result == 0 ? 0 : 1;
 }
